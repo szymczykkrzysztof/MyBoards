f318bb534b50ab88e30f5d6aeb7324c42152176b
krzysztof <krzysztof.szymczyk@yahoo.com>
2024-07-25 21:57:25 +0200

    End of section 10

--- MyBoards/Program.cs
+++ MyBoards/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyBoards.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<MyBoardsContext>(opt=>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("MyBoardsConnectionString")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
